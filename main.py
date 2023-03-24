14a607e167e76f6f2457f728ae34fee85ba53636
fu-kunn <sky53908@gmail.com>
2023-03-24 16:14:38 +0900

    [Add]パラメータの追加

--- main.py
+++ main.py
@@ -15,13 +15,29 @@ YOUTUBE_API_VERSION = "v3"
 # def youtube_search(options):
 youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
 
-q = 'Python'
-max_results = 50
+"""
+■パラメータ
+q:検索クエリ
+part:リソースのプロパティをカンマ区切りでリスト化
+viewCount:再生回数の多い順に並べる
+video:動画
+maxResults:検索結果の最大数
+"""
+
+
+q = 'Python 自動化'
+max_results = 30
 
 response = youtube.search().list(
     q=q,
     part="id,snippet",
+    order='viewCount',
+    type='video',
     maxResults=max_results
 ).execute()
 
-print(response)
+# print(response)
+
+items = response['items']
+item = items[0]
+print(item)
