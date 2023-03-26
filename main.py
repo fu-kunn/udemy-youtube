b84621e2f4191dfb63b594f57accaa2d413db7a2
fu-kunn <sky53908@gmail.com>
2023-03-26 10:54:45 +0900

    [Add]関数化

--- main.py
+++ main.py
@@ -50,69 +50,70 @@ YOUTUBE_API_VERSION = "v3"
 
 youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
 
+
+# 関数化
 df_video = video_search(youtube, q='Python 自動化', max_results=30)
-# print(df_video[:3])
 
-"""
-・uniqueで重複を削除
-・tolistでレスポンスをリスト化
-"""
-channel_ids = df_video['channel_id'].unique().tolist()
-# print(channel_ids)
+def get_results(df_video, threshold=5000):
+    """
+    ・uniqueで重複を削除
+    ・tolistでレスポンスをリスト化
+    """
+    channel_ids = df_video['channel_id'].unique().tolist()
 
-"""
-idはカンマ区切りリストの形式
-statisticsはチャンネルの統計情報
-fildsは特定のプロパティだけを返す
-　➡︎statisticsの中の特定の情報（登録者数）を返すように使う
-"""
 
-subscriber_list = youtube.channels().list(
-    id=','.join(channel_ids),
-    part="statistics",
-    fields='items(id, statistics(subscriberCount))'
-).execute()
+    """
+    idはカンマ区切りリストの形式
+    statisticsはチャンネルの統計情報
+    fildsは特定のプロパティだけを返す
+    　➡︎statisticsの中の特定の情報（登録者数）を返すように使う
+    """
 
-subscribers = []
-for item in subscriber_list['items']:
-    subscriber = {}
-    subscriber['channel_id'] = item['id']
-    subscriber['subscriber_count'] = int(item['statistics']['subscriberCount'])
-    subscribers.append(subscriber)
+    subscriber_list = youtube.channels().list(
+        id=','.join(channel_ids),
+        part="statistics",
+        fields='items(id, statistics(subscriberCount))'
+    ).execute()
 
-df_subscribers = pd.DataFrame(subscribers)
-df = pd.merge(left=df_video, right=df_subscribers, on='channel_id')
-df_extracted = df[df['subscriber_count'] < 10000]
-video_ids = df_extracted['video_id'].tolist()
+    subscribers = []
+    for item in subscriber_list['items']:
+        subscriber = {}
+        subscriber['channel_id'] = item['id']
+        subscriber['subscriber_count'] = int(item['statistics']['subscriberCount'])
+        subscribers.append(subscriber)
 
-"""
-snippet(title):動画のタイトル
-statistics(viewCount):チャンネルの再生回数
-"""
-videos_list = youtube.videos().list(
-    id=','.join(video_ids),
-    part='snippet, statistics',
-    fields='items(id, snippet(title), statistics(viewCount))'
-).execute()
-# print(videos_list['items'][:3])
+    df_subscribers = pd.DataFrame(subscribers)
+    df = pd.merge(left=df_video, right=df_subscribers, on='channel_id')
+    df_extracted = df[df['subscriber_count'] < threshold]
+    video_ids = df_extracted['video_id'].tolist()
 
-videos_info = []
-items = videos_list['items']
-for item in items:
-    video_info = {}
-    video_info['video_id'] = item['id']
-    video_info['title'] = item['snippet']['title']
-    video_info['view_count'] = item['statistics']['viewCount']
-    videos_info.append(video_info)
+    """
+    snippet(title):動画のタイトル
+    statistics(viewCount):チャンネルの再生回数
+    """
+    videos_list = youtube.videos().list(
+        id=','.join(video_ids),
+        part='snippet, statistics',
+        fields='items(id, snippet(title), statistics(viewCount))'
+    ).execute()
 
-df_videos_info = pd.DataFrame(videos_info)
-# print(df_videos_info)
+    videos_info = []
+    items = videos_list['items']
+    for item in items:
+        video_info = {}
+        video_info['video_id'] = item['id']
+        video_info['title'] = item['snippet']['title']
+        video_info['view_count'] = item['statistics']['viewCount']
+        videos_info.append(video_info)
 
-results = pd.merge(left=df_extracted, right=df_videos_info, on='video_id')
-# print(results)
+    df_videos_info = pd.DataFrame(videos_info)
 
-"""
-グラフの列順を変更
-"""
-results = results.loc[:, ['video_id', 'title', 'view_count', 'subscriber_count', 'channel_id']]
-print(results)
+    results = pd.merge(left=df_extracted, right=df_videos_info, on='video_id')
+    # グラフの列順を変更
+    results = results.loc[:, ['video_id', 'title', 'view_count', 'subscriber_count', 'channel_id']]
+
+    return results
+
+df_video = video_search(youtube, q='Python Excel', max_results=50)
+test = get_results(df_video, threshold=5000)
+print(test)
